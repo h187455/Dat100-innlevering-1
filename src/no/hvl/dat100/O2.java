276df0534c1127357f9df1de87c89e16f77e2730
h187455 <187455@stud.hvl.no>
2024-10-01 13:46:33 +0200

    10 karakterar

--- src/no/hvl/dat100/O2.java
+++ src/no/hvl/dat100/O2.java
@@ -2,30 +2,32 @@ package no.hvl.dat100;
 import java.util.Scanner;
 public class O2 {
 	public static void main(String[] args) {
-		Scanner objekt = new Scanner(System.in);
-		System.out.println("Skriv inn eit tal:");
-		String tal = objekt.nextLine();
-		int poeng=Integer.valueOf(tal);
-		if(poeng<=100 && poeng>=90) {
-			System.out.print("A");
-		}
-		if(poeng<90 && poeng>=80) {
-			System.out.print("B");
-		}
-		if(poeng<80 && poeng>=60) {
-			System.out.print("C");
-		}
-		if(poeng<60 && poeng>=50) {
-			System.out.print("D");
-		}
-		if(poeng<50 && poeng>=40) {
-			System.out.print("E");
-		}
-		if(poeng<40 && poeng>=0) {
-			System.out.print("F");
-		}
-		if(poeng>100 || poeng<0) {
-			System.out.print("ugyldig poengsum");
+		for(int i=0; i<10; i++) {
+			Scanner objekt = new Scanner(System.in);
+			System.out.println("Skriv inn eit tal:");
+			String tal = objekt.nextLine();
+			int poeng=Integer.valueOf(tal);
+			if(poeng<=100 && poeng>=90) {
+				System.out.print("A");
+			}
+			if(poeng<90 && poeng>=80) {
+				System.out.print("B");
+			}
+			if(poeng<80 && poeng>=60) {
+				System.out.print("C");
+			}
+			if(poeng<60 && poeng>=50) {
+				System.out.print("D");
+			}
+			if(poeng<50 && poeng>=40) {
+				System.out.print("E");
+			}
+			if(poeng<40 && poeng>=0) {
+				System.out.print("F");
+			}
+			if(poeng>100 || poeng<0) {
+				System.out.print("ugyldig poengsum");
+			}
 		}
 	}
 
